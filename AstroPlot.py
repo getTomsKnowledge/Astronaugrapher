d1c990b85ca1f44c5c20869acb5a77322c24b430
T.W. <twest625@gmail.com>
2025-02-21 23:08:20 -0500

    [ADD] AstroSuite upload
    Astronaugrapher.py (main)
    AstroGUI.py (GUI/HMI)
    AstroQuery (JPL Horizons manager)
    AstroSolve (leapfrog integration)
    AstroPlot (plotter, MatPlotLib)
    AstroCONST (constants)

---
+++ AstroPlot.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@project: Astronaugrapher
+@author: Tom W and Chat G
+@date: 02/21/2025
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class AstroPlot:
+    def __init__(self, parent_frame):
+        self.parent_frame = parent_frame
+
+    def plot_trajectories(self, trajectories, body_names):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        for i, name in enumerate(body_names):
+            traj = trajectories[:, i, :]
+            ax.plot(traj[:, 0], traj[:, 1], traj[:, 2], label=name)
+        ax.legend()
+        self._embed_plot(fig)
+
+    def _embed_plot(self, fig):
+        for widget in self.parent_frame.winfo_children():
+            widget.destroy()
+        canvas = FigureCanvasTkAgg(fig, self.parent_frame)
+        canvas.get_tk_widget().pack(fill='both', expand=True)
+        canvas.draw()
