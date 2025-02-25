bf7d745779c19a4d6184e0b1f7f1494cd1cf456f
T.W. <twest625@gmail.com>
2025-02-24 23:06:00 -0500

    [DEBUG] index serving coming right up!

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('/'));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
